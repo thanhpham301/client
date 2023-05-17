4dbac55bb75bf36825c66194fc98d86d818cd26f
thanhpham301 <thanhpham3011993@gmail.com>
2023-05-17 18:16:19 +0700

    done

--- src/service/orderService.js
+++ src/service/orderService.js
@@ -1,15 +1,18 @@
 export const orderService = async (item) => {
-    console.log(item)
-    const requesOptions = {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json",
-        },
-        body: JSON.stringify(item)
-    }
-    const response = await fetch("http://localhost:8080/api/v1/ordered", requesOptions)
-    console.log(response)
-    const { data } = await response.json();
-    console.log(data);
-    return data;
-}
+  console.log(item);
+  const requesOptions = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(item),
+  };
+  const response = await fetch(
+    "http://localhost:8080/api/v1/ordered",
+    requesOptions
+  );
+  console.log(response);
+  const { data } = await response.json();
+  console.log(data);
+  return data;
+};
